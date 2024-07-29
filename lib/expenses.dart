4109550d0a071d51802334ac8c685089ba07c60a
anklutch <nguyenngocanh1032002@gmail.com>
2024-07-29 18:11:31 +0900

    Update expenses.dart

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,20 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:expensetrackapp/models/expense.dart';
 
-class Expenses extends StatefulWidget{
-  const Expenses ({super.key});
-@override
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+  @override
   State<StatefulWidget> createState() {
-  return _ExpensesState();
+    return _ExpensesState();
+  }
 }
-}
+
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'FLutter course',
+      amount: 19.99,
+      date: DateTime.now(),
+      catetory: Catetory.learn
+    ),
+    Expense(
+      title: 'Deadpool',
+      amount: 5.12,
+      date: DateTime.now(),
+      catetory: Catetory.travel
+    ),
+    Expense(
+      title: 'VietNam',
+      amount: 210,
+      date: DateTime.now(),
+      catetory: Catetory.work
+    )
+  ];
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body:
-      Column(children: [
-          Text('The chart'),
-          Text('Expenses list ...')
-    ],),
+    return const Scaffold(
+      body: Column(
+        children: [Text('The chart'), Text('Expenses list ...')],
+      ),
     );
   }
 }
